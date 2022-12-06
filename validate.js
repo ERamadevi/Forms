ebfe45562af6467292bd5d0d386f19993f4885ba
ERamadevi <eramav999@gmail.com>
2022-12-06 11:54:07 +0530

    new changes

---
+++ validate.js
@@ -0,0 +1,9 @@
+function validateForm(){
+       
+	  var fname= document.getElementById("fname");
+	  var lname= document.getElementById("lname");
+	  var userid= document.getElementById("userid");
+	  var password= document.getElementById("password");
+	  console.log(fname.value);
+	  return false;
+}
